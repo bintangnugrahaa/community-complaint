b008de81de04946a13fbba79ea23d7bfaf183b91
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-07-20 12:09:29 +0700

    add resident model and migration

---
+++ app/Models/Resident.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resident extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'avatar',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
