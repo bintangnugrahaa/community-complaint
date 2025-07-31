a0da0f1f5eafc01eba5723f43e07cf182119e61f
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-07-31 14:10:39 +0700

    implement home page with categories and reports

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/app/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/app/css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
 
